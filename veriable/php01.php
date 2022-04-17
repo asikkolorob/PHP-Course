ca457e91cc7c92fc2d3e420b46bdd4b00d3c0d82
AshikSovware <asikrahman4545@gmail.com>
2022-04-18 02:43:30 +0600

    Veriable Introduction

---
+++ veriable/php01.php
@@ -0,0 +1,14 @@
+<?php
+ // Veriable
+ $name = "Bangladesh"; //? String
+ $age = 19; //? Intiger
+ $quetion = 'How are you?';
+
+//  $bangladsh_name = 'Banglash'; //? veriable name example
+
+echo 'Hello '.$name; //? String concat 1
+echo "\n"; //? New Line always the way
+echo "Hello $name"; //? String concat 2
+
+echo "\n";
+echo "Hello {$name} , {$quetion}"; // This Best Prectice
